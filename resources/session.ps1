8e52065d9dccf3edec3c0fc4a03b39eadef8a39f
Valentin Pettmann <vet@qlik.com>
2020-03-17 12:16:02 +0100

    commit on 'Master'

--- resources/session.ps1
+++ resources/session.ps1
@@ -76,3 +76,61 @@ function Get-QlikSession
 		return $response
 	}
 }
+
+<#
+	.SYNOPSIS
+		Kills the specified session on the specified Virtual Proxy
+	
+	.DESCRIPTION
+		This takes a session ID as a parameter to remove it from the default proxy or the specified proxy.
+		https://help.qlik.com/en-US/sense-developer/November2018/apis/ProxyAPI/OpenAPI_Main.generated.html?page=16
+	
+	.PARAMETER SessionId
+		This is to Specify the Id of the session to be killed
+	
+	.PARAMETER virtualProxyPrefix
+		Specifies the Virtual Proxy where to kill the sessions from
+		
+	.EXAMPLE
+				PS C:\> Remove-QlikSession "7b8ab85e-0c85-4fcb-8e56-4ead683153fb"
+	.EXAMPLE
+				PS C:\> Remove-QlikSession -SessionId "7b8ab85e-0c85-4fcb-8e56-4ead683153fb" -virtualProxyPrefix "/ProxyX1"
+	.EXAMPLE
+				PS C:\> Get-QlikSession -userDirectory Domain -userId Marc | Foreach{Remove-QlikSession}
+	
+	.NOTES
+		Additional information about the Session API can be found
+		https://help.qlik.com/en-US/sense-developer/November2018/apis/ProxyAPI/OpenAPI_Main.generated.html#19b1cf4a56294022A146C978a46f3a59
+		https://help.qlik.com/en-US/sense-developer/November2018/Subsystems/ProxyServiceAPI/Content/Sense_ProxyServiceAPI/ProxyServiceAPI-Session-Module-API-Session-Get.htm
+
+#>
+function Remove-QlikSession
+{
+	[CmdletBinding()]
+	param
+	(
+		[Parameter(ParameterSetName = 'SessionId',
+				   Mandatory = $true,
+				   ValueFromPipelineByPropertyName = $true,
+				   ValueFromPipeline=$true,
+				   Position = 0)]
+		[string]$SessionId,
+		[Alias('vp')]
+		[string]$virtualProxyPrefix
+	)
+	
+	PROCESS
+	{
+		$proxy = Get-QlikProxy local
+		$prefix = "https://$($proxy.serverNodeConfiguration.hostName):$($proxy.settings.restListenPort)/qps"
+		if ($PSBoundParameters.ContainsKey("virtualProxyPrefix")) { $prefix = "$($prefix)/$virtualProxyPrefix" }
+		$path = "$prefix/session/$SessionId" 
+		
+		try
+		{
+			$response = Invoke-QlikDelete $path
+		}
+		catch { $response = $null }
+		return $response
+	}
+}
