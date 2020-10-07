1a962dd89edf3160295dca147fe6a911379c24f9
Adam Haydon <adam.haydon@qlik.com>
2020-10-07 14:31:24 +0100

    Fix code formatting

--- tests/core.tests.ps1
+++ tests/core.tests.ps1
@@ -5,15 +5,15 @@ Describe 'CallRestUri' {
     InModuleScope Qlik-Cli {
         Context 'when uploading or downloading files' {
             BeforeAll {
-                Mock Invoke-WebRequest -Verifiable {}
+                Mock Invoke-WebRequest -Verifiable { }
                 Mock Invoke-RestMethod { throw 'Invoke-RestMethod should not be used for file transfers' }
                 $script:prefix = 'https://localhost'
-                $script:api_params = @{}
+                $script:api_params = @{ }
                 $script:rawOutput = $true
             }
 
             it 'should use Invoke-WebRequest' {
-                CallRestUri GET /qrs/download/scriptlog @{OutFile='TestDrive:\script.log'}
+                CallRestUri GET /qrs/download/scriptlog @{ OutFile = 'TestDrive:\script.log' }
 
                 Assert-VerifiableMock
             }
