f8d37874b241eb6701b0e96855a454faa9c4b07f
viktor.petrischev <viktor.petrischev@x5.ru>
2022-11-01 15:31:34 +0300

    Fix identation

--- resources/object.ps1
+++ resources/object.ps1
@@ -64,21 +64,21 @@ function Update-QlikObject {
     param (
         [parameter(Mandatory = $true, ValueFromPipelinebyPropertyName = $True, Position = 0)]
         [string]$id,
-		
-		[string]$name,
+
+        [string]$name,
         [object]$owner,
         [bool]$approved
     )
 
     PROCESS {
         $obj = Get-QlikObject $id -raw
-        If ( $name ) { 
-			$obj.name = $name 
-		}
-		If ($psBoundParameters.ContainsKey("approved")) { $obj.approved = $approved }
-		
-		if ($PSBoundParameters.ContainsKey("owner")) { $obj.owner = GetUser $owner }
-		
+        If ( $name ) {
+            $obj.name = $name
+        }
+        If ($psBoundParameters.ContainsKey("approved")) { $obj.approved = $approved }
+
+        If ($PSBoundParameters.ContainsKey("owner")) { $obj.owner = GetUser $owner }
+
         $json = $obj | ConvertTo-Json -Compress -Depth 10
         return Invoke-QlikPut "/qrs/app/object/$id" $json
     }
